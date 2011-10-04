6b1c2628c40d0785f528b552b80ebe2c342540e3
Chad Lowe <chad.lowe@ldssystems.co.uk>
2011-10-04 14:44:02 +0100

    Signed-off-by: Chad Lowe <chad.lowe@ldssystems.co.uk>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 function geo(){
   var suc = function(p) {
     document.getElementById("location").innerHTML = p.coords.latitude + " " + p.coords.longitude;
+    alert(p.coords.latitude + " " + p.coords.longitude);
   };
   var locFail = function() {
     document.getElementById("location").innerHTML = "So sorry but your gps isnt on";
   };
   navigator.geolocation.getCurrentPosition(suc, locFail);
+}
+function vibratephone(){
+  alert("vibrate");
+  navigator.notification.vibrate(0);
 }
